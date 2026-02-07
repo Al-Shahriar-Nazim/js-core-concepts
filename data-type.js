43c2756e6f16f326288ad67cc187052f692cfe6f
Al-Shahriar-Nazim <nazimking7634@gmail.com>
2026-02-07 14:39:12 +0600

    data-types

---
+++ data-type.js
@@ -0,0 +1,29 @@
+//js is a dynamically typed language.
+let data = 2;
+console.log(typeof data);
+//output: number
+
+data = "hello";
+console.log(typeof data);
+//output: string
+
+data = true;
+console.log(typeof data);
+//outPUt: boolean
+
+data = {name:"nazim" , age:23};
+console.log(typeof data);
+//output: object
+
+data =[2,20];
+console.log(typeof data);
+// output : object
+
+
+data = undefined;
+console.log(typeof data);
+//output: undefined
+
+data = null;  
+console.log(typeof data);
+//output: object
