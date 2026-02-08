588622fdc24da5ecaf50a06ef1862299afc0a498
Al-Shahriar-Nazim <nazimking7634@gmail.com>
2026-02-08 16:33:34 +0600

    added increment

---
+++ increment.js
@@ -0,0 +1,11 @@
+let counter = 0 ;
+console.log(counter = counter + 1);
+// outPut : 1
+//prefix increment
+console.log(counter +=1);
+// outPut : 2
+
+//postfix increment
+console.log(counter++);
+console.log(counter)
+// outPut : 3
